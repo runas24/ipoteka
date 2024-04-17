d4af25aa803d1c226b0c2b0b421c4201bf6af198
runas24 <166917824+runas24@users.noreply.github.com>
2024-04-17 11:31:56 +0500

    Update script.js

--- script.js
+++ script.js
@@ -14,31 +14,10 @@ document.getElementById("loanForm").addEventListener("submit", function(event) {
     submitButton.disabled = true; // Отключаем кнопку во время загрузки
 
     var fullName = document.getElementById("fullName").value;
-    var loanDate = document.getElementById("loanDate").value;
+    var date = document.getElementById("date").value;
     var desiredAmount = parseFloat(document.getElementById("desiredAmount").value.replace(/\D/g, ''));
     var creditBurden = parseFloat(document.getElementById("creditBurden").value.replace(/\D/g, ''));
     var pensionContributions = parseFloat(document.getElementById("pensionContributions").value.replace(/\D/g, ''));
 
-    // Рассчитываем максимальную сумму кредита
-    // Больший процент от пенсионных отчислений увеличивает максимальную сумму кредита
-    var maxLoanAmount = desiredAmount - creditBurden + (pensionContributions * 6 * 2);
-
-    // Отправляем данные на Google Apps Script
-    google.script.run.withSuccessHandler(function() {
-        submitButton.disabled = false; // Включаем кнопку после загрузки
-        overlay.style.display = "none"; // Скрываем оверлей
-
-        alert("Данные успешно отправлены!");
-    }).sendDataToGoogleSheet(fullName, loanDate, maxLoanAmount);
-
-    setTimeout(function() {
-        submitButton.disabled = false; // Включаем кнопку после загрузки
-        overlay.style.display = "none"; // Скрываем оверлей
-
-        // Скрыть максимальную сумму кредита
-        // document.getElementById("maxLoanAmount").innerText = "Максимальная сумма кредита: " + maxLoanAmount.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, " ") + " тенге";
-        // Предполагаю, что результат все еще нужно запомнить, поэтому не удаляем вычисления maxLoanAmount
-
-        document.getElementById("overlay").innerHTML = '<img src="result.gif" alt="Result"><div id="loadingText">Ваша заявка на рассмотрении</div>';
-    }, 10000); // Результат появится через 10 секунд (10000 миллисекунд)
+    google.script.run.doGet(fullName, date, desiredAmount, creditBurden, pensionContributions);
 });
