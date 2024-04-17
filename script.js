4af2b5017aa81565eb4a927384c79b35bbaaf4f7
runas24 <166917824+runas24@users.noreply.github.com>
2024-04-17 11:17:14 +0500

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.getElementById("loanForm").addEventListener("submit", function(event) {
     submitButton.disabled = true; // Отключаем кнопку во время загрузки
 
     var fullName = document.getElementById("fullName").value;
-    var dateOfBirth = document.getElementById("dateOfBirth").value;
+    var loanDate = document.getElementById("loanDate").value;
     var desiredAmount = parseFloat(document.getElementById("desiredAmount").value.replace(/\D/g, ''));
     var creditBurden = parseFloat(document.getElementById("creditBurden").value.replace(/\D/g, ''));
     var pensionContributions = parseFloat(document.getElementById("pensionContributions").value.replace(/\D/g, ''));
@@ -23,28 +23,14 @@ document.getElementById("loanForm").addEventListener("submit", function(event) {
     // Больший процент от пенсионных отчислений увеличивает максимальную сумму кредита
     var maxLoanAmount = desiredAmount - creditBurden + (pensionContributions * 6 * 2);
 
-    // Отправляем данные на сервер
-    var formData = new FormData();
-    formData.append('fullName', fullName);
-    formData.append('dateOfBirth', dateOfBirth);
-    formData.append('desiredAmount', desiredAmount);
-    formData.append('creditBurden', creditBurden);
-    formData.append('pensionContributions', pensionContributions);
-    formData.append('maxLoanAmount', maxLoanAmount);
+    // Отправляем данные на Google Apps Script
+    google.script.run.submitForm(fullName, loanDate, maxLoanAmount);
 
-    fetch('https://script.google.com/macros/s/AKfycbxGh8pH6EOSuN6Ys0vov4Bex-pnyd43S1or2w81LTZoZWM8-nG7sDwyxA9OKs5DXsh4/exec', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.text())
-    .then(data => {
-        console.log(data);
+    setTimeout(function() {
         submitButton.disabled = false; // Включаем кнопку после загрузки
         overlay.style.display = "none"; // Скрываем оверлей
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        submitButton.disabled = false; // Включаем кнопку после загрузки
-        overlay.style.display = "none"; // Скрываем оверлей
-    });
+
+        // Сообщаем пользователю, что заявка отправлена
+        alert("Заявка отправлена на рассмотрение. Максимальная сумма кредита: " + maxLoanAmount.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, " ") + " тенге");
+    }, 10000); // Результат появится через 10 секунд (10000 миллисекунд)
 });
