2f50a3ad30d85e5d937e04151815ccb7920a7329
runas24 <166917824+runas24@users.noreply.github.com>
2024-04-17 11:57:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,14 @@
-function formatCurrency(input) {
-    var value = input.value.replace(/\D/g, '');
-    var formattedValue = new Intl.NumberFormat('ru-RU').format(value);
-    input.value = formattedValue;
-}
-
 document.getElementById("loanForm").addEventListener("submit", function(event) {
     event.preventDefault();
 
-    var overlay = document.getElementById("overlay");
-    overlay.style.display = "flex"; // Показываем оверлей
-
-    var submitButton = document.querySelector('button[type="submit"]');
-    submitButton.disabled = true; // Отключаем кнопку во время загрузки
-
     var fullName = document.getElementById("fullName").value;
-    var date = document.getElementById("date").value;
     var desiredAmount = parseFloat(document.getElementById("desiredAmount").value.replace(/\D/g, ''));
     var creditBurden = parseFloat(document.getElementById("creditBurden").value.replace(/\D/g, ''));
     var pensionContributions = parseFloat(document.getElementById("pensionContributions").value.replace(/\D/g, ''));
+    var date = document.getElementById("date").value;
 
-    // Рассчитываем максимальную сумму кредита
-    // Больший процент от пенсионных отчислений увеличивает максимальную сумму кредита
     var maxLoanAmount = desiredAmount - creditBurden + (pensionContributions * 6 * 2);
 
-    // Отправляем данные на сервер через fetch
     fetch('https://script.google.com/macros/s/AKfycbxGh8pH6EOSuN6Ys0vov4Bex-pnyd43S1or2w81LTZoZWM8-nG7sDwyxA9OKs5DXsh4/exec', {
         method: 'POST',
         headers: {
@@ -31,23 +16,19 @@ document.getElementById("loanForm").addEventListener("submit", function(event) {
         },
         body: JSON.stringify({
             fullName: fullName,
-            date: date,
-            desiredAmount: desiredAmount,
-            creditBurden: creditBurden,
-            pensionContributions: pensionContributions,
-            maxLoanAmount: maxLoanAmount
+            maxLoanAmount: maxLoanAmount,
+            date: date
         })
     })
     .then(response => response.json())
     .then(data => {
-        // Обработка ответа, если нужно
-        console.log(data);
+        if (data.success) {
+            alert("Документ успешно создан!");
+        } else {
+            alert("Произошла ошибка при создании документа.");
+        }
     })
     .catch(error => {
-        console.error('Ошибка:', error);
-    })
-    .finally(() => {
-        submitButton.disabled = false; // Включаем кнопку после загрузки
-        overlay.style.display = "none"; // Скрываем оверлей
+        alert("Произошла ошибка: " + error.message);
     });
 });
