87ed2d8719f3d3b4188fa92ba76fe12e9cc766fe
runas24 <166917824+runas24@users.noreply.github.com>
2024-04-17 12:15:46 +0500

    Update script.js

--- script.js
+++ script.js
@@ -7,51 +7,17 @@ function formatCurrency(input) {
 document.getElementById("loanForm").addEventListener("submit", function(event) {
     event.preventDefault();
 
-    var overlay = document.getElementById("overlay");
-    overlay.style.display = "flex"; // Показываем оверлей
-
-    var submitButton = document.querySelector('button[type="submit"]');
-    submitButton.disabled = true; // Отключаем кнопку во время загрузки
-
+    var fullName = document.getElementById("fullName").value;
+    var loanDate = document.getElementById("loanDate").value;
     var desiredAmount = parseFloat(document.getElementById("desiredAmount").value.replace(/\D/g, ''));
     var creditBurden = parseFloat(document.getElementById("creditBurden").value.replace(/\D/g, ''));
     var pensionContributions = parseFloat(document.getElementById("pensionContributions").value.replace(/\D/g, ''));
-    var fullName = document.getElementById("fullName").value;
-    var date = document.getElementById("date").value;
 
     // Рассчитываем максимальную сумму кредита
     // Больший процент от пенсионных отчислений увеличивает максимальную сумму кредита
     var maxLoanAmount = desiredAmount - creditBurden + (pensionContributions * 6 * 2);
 
-    fetch('https://script.google.com/macros/s/AKfycbxGh8pH6EOSuN6Ys0vov4Bex-pnyd43S1or2w81LTZoZWM8-nG7sDwyxA9OKs5DXsh4/exec', {
-       method: 'POST',
-       headers: {
-           'Content-Type': 'application/json',
-       },
-       body: JSON.stringify({
-           fullName: fullName,
-           date: date,
-           maxLoanAmount: maxLoanAmount.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, " ")
-       }),
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Произошла ошибка при отправке запроса.');
-        }
-        return response.json();
-    })
-    .then(data => {
-        if (data.success) {
-            console.log('Данные успешно отправлены.');
-        } else {
-            console.error('Ошибка при обработке данных на сервере:', data.error);
-        }
-    })
-    .catch(error => {
-        console.error('Произошла ошибка:', error);
-    })
-    .finally(() => {
-        submitButton.disabled = false; // Включаем кнопку после загрузки
-        overlay.style.display = "none"; // Скрываем оверлей
+    google.script.run.submitForm(fullName, loanDate, maxLoanAmount, function(response) {
+        document.getElementById("resultMessage").innerText = response;
     });
 });
