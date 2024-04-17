48c6ae78f7d8aa1e8a69cfcac5acef8edea49e4a
runas24 <166917824+runas24@users.noreply.github.com>
2024-04-17 12:33:22 +0500

    Update script.js

--- script.js
+++ script.js
@@ -29,6 +29,8 @@ document.getElementById("loanForm").addEventListener("submit", function(event) {
     .then(response => response.text())
     .then(data => {
         document.getElementById("resultMessage").textContent = data;
+        document.getElementById("viewResult").style.display = "inline"; // Показываем кнопку "Посмотреть результат"
+        document.getElementById("viewResult").setAttribute("href", "https://drive.google.com/drive/folders/1xHv24Egg1wr9OMKeBVY5XP7oP7h6fTJ8");
     })
     .catch(error => {
         console.error('Error:', error);
