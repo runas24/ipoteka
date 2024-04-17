5f4021492d91bc1503fb21c025a4ac852b80d78e
runas24 <166917824+runas24@users.noreply.github.com>
2024-04-17 11:43:53 +0500

    Update script.js

--- script.js
+++ script.js
@@ -14,10 +14,20 @@ document.getElementById("loanForm").addEventListener("submit", function(event) {
     submitButton.disabled = true; // Отключаем кнопку во время загрузки
 
     var fullName = document.getElementById("fullName").value;
-    var date = document.getElementById("date").value;
+    var loanDate = document.getElementById("loanDate").value;
     var desiredAmount = parseFloat(document.getElementById("desiredAmount").value.replace(/\D/g, ''));
     var creditBurden = parseFloat(document.getElementById("creditBurden").value.replace(/\D/g, ''));
     var pensionContributions = parseFloat(document.getElementById("pensionContributions").value.replace(/\D/g, ''));
 
-    google.script.run.doGet(fullName, date, desiredAmount, creditBurden, pensionContributions);
+    google.script.run.withSuccessHandler(function(maxLoanAmount) {
+        setTimeout(function() {
+            submitButton.disabled = false; // Включаем кнопку после загрузки
+            overlay.style.display = "none"; // Скрываем оверлей
+
+            document.getElementById("overlay").innerHTML = '<img src="result.gif" alt="Result"><div id="loadingText">Ваша заявка предварительно одобрена</div>';
+        }, 10000); // Результат появится через 10 секунд (10000 миллисекунд)
+        
+        // Отправляем данные на Google Документ
+        google.script.run.addToGoogleDoc(fullName, loanDate, maxLoanAmount);
+    }).calculateMaxLoanAmount(desiredAmount, creditBurden, pensionContributions);
 });
